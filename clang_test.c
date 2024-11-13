610dd7d20bda38b5ef0f5bd7a4455983a74356e3
Shanazar <snizamaddinow@gmail.com>
2024-11-13 23:50:14 +0300

    clang-format test file

---
+++ clang_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a = 5;
+  int b = 10;
+  if (a < b) {
+    printf("a is less than b\n");
+  } else {
+    printf("a is not less than b\n");
+  }
+  return 0;
+}
